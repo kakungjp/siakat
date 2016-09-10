09f790389b778fd89376054d8f3e0426b5c19c45
kakungjp <kakungjp@gmail.com>
2016-09-10 21:15:28 +0700

    Template In Views

---
+++ application/views/frontend/template/sidebar.php
@@ -0,0 +1,50 @@
+    <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+    	<div class="sidebar-wrapper">
+            <div class="logo">
+                <a class="simple-text">
+                    Nama User
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="dashboard.html">
+                        <i class="pe-7s-graph"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="user.html">
+                        <i class="pe-7s-news-paper"></i>
+                        <p>Daftar Hasil Studi</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="table.html">
+                        <i class="pe-7s-note2"></i>
+                        <p>KRS/KHS</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="typography.html">
+                        <i class="pe-7s-date"></i>
+                        <p>Jadwal Kuliah</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="icons.html">
+                        <i class="pe-7s-user"></i>
+                        <p>Data Mahasiswa</p>
+                    </a>
+                </li>
+            </ul>
+    	</div>
+    </div>
